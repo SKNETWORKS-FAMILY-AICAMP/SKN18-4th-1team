90b94d2c2c07b10fa3913c45b5fdffbf509a2e85
sonjuyeong-00 <sonju9638@gmail.com>
2025-11-04 17:58:31 +0900

    feat: 파일  구성 수정

---
+++ Feature-rag/Langgraph/init_state.py
@@ -0,0 +1,7 @@
+from typing import Any, Dict, List, TypedDict
+
+
+class GraphState(TypedDict):
+    # 사용자 input 
+    question: str
+
--- Feature-rag/Langgraph/nodes/classify_node.py
+++ Feature-rag/Langgraph/nodes/classify_node.py
@@ -1,1 +1,16 @@
 # 증상을 의미하는 질문인지, 쓸데없는 질문인지 판별하는 node
+from init_state import GraphState
+from Langgraph.graph import END
+
+def classify_node(state: GraphState) -> GraphState:
+    return {
+        **state
+    }
+    
+        
+        
+def classify_quit(state: GraphState) -> str:
+    if state.get("need_quit", False):
+        return END
+    return "search_node"
+        
